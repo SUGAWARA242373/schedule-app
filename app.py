878e6e468598a086ad0963c9837daed24aa41734
SUGAWARA242373 <t.sugawara@shell.com>
2026-06-19 14:57:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -169,8 +169,9 @@ def draw(d):
 
     
 with c3:
-        val = st.text_area("", key=f"sch_{d}", placeholder="予定", height=70)
+        val = st.text_input("", key=f"sch_{d}", placeholder="予定")
         st.session_state.schedule[d] = val
+
 
 
 # 表（左右2列）
