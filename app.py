444dfc10a39e7b2def9f7b17a4dcfe216383fee0
SUGAWARA242373 <t.sugawara@shell.com>
2026-06-19 14:58:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ def draw(d):
         st.session_state.duty[d] = val
 
     
-with c3:
+　　with c3:
         val = st.text_input("", key=f"sch_{d}", placeholder="予定")
         st.session_state.schedule[d] = val
 
