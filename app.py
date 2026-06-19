62a25c049afa41cc7a9fc3c3a630d118730d1d0b
SUGAWARA242373 <t.sugawara@shell.com>
2026-06-19 17:51:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -167,8 +167,7 @@ def draw(d):
         val = st.text_input("", key=f"duty_{d}", placeholder="当番")
         st.session_state.duty[d] = val
         
-   
-　　with c3:
+    with c3:
     val = st.text_area(
         "",
         key=f"sch_{d}",
