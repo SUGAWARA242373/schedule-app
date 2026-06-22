6b4ad1c9889b03aea6e03cbe4ed6d31775f7045e
SUGAWARA242373 <t.sugawara@shell.com>
2026-06-22 21:23:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -56,11 +56,6 @@ if os.path.exists(data_file) and not st.session_state.get("loaded", False):
             st.session_state[k] = v
     st.session_state.loaded = True
 
-temp = st.sidebar.selectbox(
-    "予定テンプレ",
-    templates,
-    key="template_select"
-)
 
 day_sel = st.sidebar.number_input(
     "日付",
